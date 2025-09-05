eba79b4c90b4449dd8cee21bd8a98cb3977614cb
PhamNhatAnh2003 <anhpham090423@gmail.com>
2025-09-05 11:12:23 +0700

    filter

--- backend/routes/BookRoutes.js
+++ backend/routes/BookRoutes.js
@@ -1,11 +1,18 @@
 import express from "express";
 import multer from "multer";
 
-import { createBook } from "../controller/bookController.js";
+import {
+  createBook,
+  getBookByName,
+  getBooksByCategoryController,
+} from "../controller/bookController.js";
 
 const router = express.Router();
 const upload = multer();
 
 router.post("/create", createBook);
+router.get("/books", getBookByName);
+router.get("/books/category/:category", getBooksByCategoryController);
+
 
 export default router;
